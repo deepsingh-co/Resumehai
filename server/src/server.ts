aea24280809da002e0e95aaea595f22a7d5f4628
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-08-09 10:57:53 +0530

    chore: configure project architecture

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,30 @@
+import "dotenv/config";
 import express from "express";
+import { env } from "./config/env.js";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
-const PORT = 5000;
+app.use(helmet());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/health", (_req, res) => {
   res.json({
     success: true,
     message: "AI Resume Maker API is running",
+    environment: env.nodeEnv,
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(env.port, () => {
+  console.log(
+    `Server running on http://localhost:${env.port}`
+  );
 });
